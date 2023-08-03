af9b7af6213b9f5f90a65986611fcf2bb9b9e706
binbinjji <yb080707@naver.com>
2023-08-03 16:01:36 +0900

    배포 테스트

--- src/main/java/com/example/gg/Controller.java
+++ src/main/java/com/example/gg/Controller.java
@@ -8,6 +8,10 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class Controller {
 
+    @GetMapping("/version")
+    public ResponseEntity<String> version_check(){
+        return ResponseEntity.ok().body("성공~! 버전 2");
+    }
     @GetMapping("/healthcheck")
     public ResponseEntity<Integer> health_check(){
         return ResponseEntity.ok().body(200);
